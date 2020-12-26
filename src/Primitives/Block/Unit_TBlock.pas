228c39dcfeb076fcab4fff425469dd1ef230b707
SkybuckFlying <skybuck2000@hotmail.com>
2020-12-26 04:43:53 +0100

    Copyright and license link added to units

--- src/Primitives/Block/Unit_TBlock.pas
+++ src/Primitives/Block/Unit_TBlock.pas
@@ -1,3 +1,10 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2019 The Bitcoin Core developers
+// Copyright (c) 2020-2020 Skybuck Flying
+// Copyright (c) 2020-2020 The Delphicoin Developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 unit Unit_TBlock;
 
 interface
